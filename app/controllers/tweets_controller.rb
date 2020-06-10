a79b921c27b9068530ae5c5ad5b5783a92b91c27
Haloren <mckennakyler@gmail.com>
2020-06-10 15:10:50 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,12 @@
 class TweetsController < ApplicationController
 
+        get '/tweets' do
+            @tweets = Tweet.all
+            erb :'tweets/tweets'
+        end
+
+        post '/tweets' do
+            
+        end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,52 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+
+        if !logged_in?
+            erb :'users/create_user'
+        else
+            redirect to '/tweets'
+        end
+
+    end
+
+    post '/signup' do
+        @user = User.new(params)
+
+        if !@user.save
+            erb :'users/create_user'
+        else
+            session[:user_id] = @user.id
+            redirect to '/tweets'
+        end
+
+    end
+
+    get '/login' do
+
+        if !logged_in?
+            erb :'users/login'
+        else
+            redirect to '/tweets'
+        end
+
+    end
+
+    post '/login' do
+        user = User.find_by(username: params[:username])
+
+        if user && user.authenticate(params[:password])
+            session[:id] = user.id
+            redirect '/guides'
+        else
+            erb :'users/login'
+        end
+
+    end
+
+    get '/logout' do
+        session.destroy
+        redirect '/'
+    end
 
 end
